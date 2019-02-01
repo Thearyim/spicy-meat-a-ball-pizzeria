52312a62fdc650913972dbbacc124719e936d9aa
ThearyIm <thearyim@gmail.com>
2019-02-01 12:44:26 -0800

    Continuing to refactor javascript data structures and to implement prototype functions.

--- js/scripts.js
+++ js/scripts.js
@@ -15,27 +15,39 @@ Pizzeria.prototype.buildYourOwn = function(ingredients, size) {
   return pizza;
 }
 
-Pizzeria.prototype.ingredient = function(id) {
-  var ingredient;
-  for (var i = 0; i < this.ingredients.length; i++) {
-    var currentIngredient = this.ingredients[i];
-    if (currentIngredient.id == id) {
-      ingredient = currentIngredient;
-      break;
-    }
-  }
-
-  return ingredient;
-}
 
 Pizzeria.prototype.getSignaturePizzas = function() {
   var supreme = new Pizza(
-    "supreme",
-    ["dough", "tomato sauce", "sausage", "bell peppers", "onion", "black olives"]);
+    ["dough", "tomato sauce", "mozzarella", "sausage", "bell peppers", "onions", "black olives"]
+  );
 
-  var hawaiian = 
+  supreme.name = "supreme";
+  supreme.description = "A traditional pizza with tomato sauce, mozzarella, sausage, bell peppers, onions and black olives"
 
-  var signaturePizzas = [supreme];
+  var hawaiian = new Pizza(
+    ["dough", "tomato sauce", "mozzarella", "ham", "pineapple"]
+  );
+
+  hawaiian.name = "hawaiian";
+  hawaiian.description = "A traditional pizza with tomato sauce, mozzarella, ham and pineapple";
+
+  var pepperoni = new Pizza(
+    ["dough", "tomato sauce", "mozzarella", "pepperoni"]
+  );
+
+  pepperoni.name = "pepperoni";
+  pepperoni.description = "A traditional pizza with tomato sauce, mozzarella, pepperoni";
+
+  var fourCheese = new Pizza(
+    "fourCheese",
+    "A traditional pizza with tomato sauce, mozzarella, and our famous 3 cheese blend",
+    ["dough", "tomato sauce", "mozzarella", "3 cheese"]
+  );
+
+  fourCheese.name = "fourCheese";
+  fourCheese.description = "A traditional pizza with tomato sauce, mozzarella, and our famous 3 cheese blend";
+
+  var signaturePizzas = [supreme, hawaiian, pepperoni, fourCheese];
 
   return signaturePizzas;
 }
@@ -54,7 +66,7 @@ Pizzeria.prototype.initialize = function() {
     "onions",
     "black olives",
     "anchovies",
-    "cheese",
+    "mozzarella",
     "3 cheese"
   ];
 
@@ -68,21 +80,77 @@ Pizzeria.prototype.initialize = function() {
 function Pizza(ingredients) {
   this.ingredients = ingredients;
   this.name;
-  this.size;
+  this.description;
   this.id;
 }
 
-function Pizza(name, ingredients) {
-  this.ingredients = ingredients;
-  this.name = name;
-  this.size;
-  this.id;
-}
-
-Pizza.prototype.getPrice = function() {
+Pizza.prototype.getIngredientsPrice = function(size) {
   var price = 0;
+  var ingredientsWithCost = [
+    "pepperoni",
+    "mushrooms",
+    "pineapple",
+    "sausage",
+    "ham",
+    "bell peppers",
+    "onions",
+    "black olives",
+    "anchovies",
+    "3 cheese"
+  ];
+
+  this.ingredients.forEach(function(ingredient) {
+    var hasPrice = ingredientsWithCost.includes(ingredient);
+
+    if (hasPrice) {
+      if (size == "small") {
+        price += 0.5;
+      }
+      else if (size == "medium") {
+        price += 0.75;
+      }
+      else {
+        price += 1.0;
+      }
+    }
+  });
+  return price;
+}
+
+Pizza.prototype.getPrice = function(size) {
+  var price = 0;
+  if (this.isSignature()) {
+    if (size == "small") {
+      price = 10.99;
+    }
+    else if (size == "medium") {
+      price = 12.99;
+    }
+    else {
+      price = 14.99;
+    }
+  }
+  else {
+    var ingredientsPrice = this.getIngredientsPrice(size);
+    if (size == "small") {
+      price = 7.99 + ingredientsPrice;
+    }
+    else if (size == "medium") {
+      price = 9.99 + ingredientsPrice;
+    }
+    else {
+      price = 11.99 + ingredientsPrice;
+    }
+  }
 
   return price;
+}
+
+Pizza.prototype.isSignature = function() {
+  var signaturePizzas = ["supreme", "hawaiian", "pepperoni", "fourCheese"];
+  var isSignaturePizza = signaturePizzas.includes(this.name);
+
+  return isSignaturePizza;
 }
 
 function showTestData() {
@@ -93,10 +161,12 @@ function showTestData() {
   var pizza = new Pizza([
     pizzeria.ingredients[1],
     pizzeria.ingredients[3],
+    pizzeria.ingredients[5],
   ]);
 
+  console.log(pizzeria.getSignaturePizzas()[0].getPrice("small"));
   console.log(pizza);
-  console.log(pizza.getPrice());
+ console.log(pizza.getPrice("medium"));
 
 }
 
