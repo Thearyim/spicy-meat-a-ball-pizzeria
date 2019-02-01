bfee3566c8c80061b1878be8a7d695638b16ca9e
ThearyIm <thearyim@gmail.com>
2019-02-01 10:37:08 -0800

    Initialize object structure and README

---
+++ README.md
@@ -0,0 +1,37 @@
+# _Spicy-Meat-a-Ball Pizzeria_
+
+#### _02/01/2019_
+
+## Author
+ _**Theary Im**_  
+ _**thearyim@gmail.com**_
+
+## Description
+**_This is a web page for my pizzeria. You can customize you pie.. Give it a try!_**
+* _ _
+* _ _
+* _ _
+
+**_In my pizzeria, prices will vary depending up the topping and the size of the pizza pie. However, just for fun
+ there's a combination of toppings that should you choose them, your toppings will be free. I can't tell you what
+ that combination is but I can give you some hints. Best of luck and enjoy your pizza!_**
+
+| Example Input| Example Output                                       |
+| :----------- | :--------------------------------------------------- |
+
+
+## Technologies Used
+* _HTML_
+* _CSS(Bootstrap)_
+* _ATOM_
+* _JS_
+* _JQuery_
+* _Paint.net_
+* _Git_
+* _Bing_
+
+## License
+MIT
+**
+
+Copyright (c) 2019 **_Theary Im_**
---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+//create pizzeria object
+function Pizzeria() {
+  this.ingredients = [];
+  this.sizes = [];
+}
+
+Pizzeria.prototype.getSignaturePizza = function() {
+  var signaturePizzas = [];
+
+  return signaturePizzas;
+}
+
+function Pizza() {
+  this.ingredients = [];
+  this.name;
+}
+
+pizzeria.prototype.initialize = function() {
+  this.sizes = [
+    new Size("small"),
+    new Size("medium"),
+    new Size("large")
+  ];
+}
+
+//function to build your own pizza
+pizzeria.prototype.buildYourOwn = function() {
+  this.pizzas = [
+    new pizza("dough"),
+  ];
+
+  this.ingredients = [
+    new Ingredient("sauce"),
+    new Ingredient("toppings"),
+    new Ingredient("cheese"),
+  ];
+}
+
+//function returns an array of ingredients that customer pick
+Pizzeria.prototype.pizzaChoice = function() {
+  var pizzaChoice = [];
+  var buildYourOwn = [];
+  var customMade = [];
+
+if (customMadePizza = true) {
+  getPizza = customMadePizza;
+}
+else {
+  getPizza = buildYourOwn;
+}
