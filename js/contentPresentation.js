ec14ad4b28f2b1b77d01a41bb60c837383cae616
ThearyIm <thearyim@gmail.com>
2019-02-02 18:34:28 -0800

    Get the tracking order pizza to track both customize and signature pizza to track correctly.

--- js/contentPresentation.js
+++ js/contentPresentation.js
@@ -3,29 +3,36 @@ function ContentPresentation(pizzeria) {
 }
 
 ContentPresentation.prototype.setPizzaSelectedEvents = function() {
+  var pizzeria = this.pizzeria;
+  var signaturePizzas = this.pizzeria.getSignaturePizzas();
   var orderTracker = this.pizzeria.orderTracker;
   var contentPresentation = this;
-  
+
   //adding click event handler to show the pizza customization option
   $("#byoContainer div > button").click(function(event) {
     var byoPizzaSize = $(event.target).attr("id").split(":")[2];
-    orderTracker.trackPizzaOrder(
-        new Pizza(["dough", "tomato sauce", "mozzarella"]),
-        byoPizzaSize
-    );
+    var pizza = new Pizza(["dough", "tomato sauce", "mozzarella"]);
+    pizza.name = "byo";
+    pizza.description = "Build-Your-Own Pizza";
+    pizza.imagePath = "img/CheesePizza.png";
+    orderTracker.trackPizzaOrder(pizza, byoPizzaSize);
 
     console.log(orderTracker);
     contentPresentation.showPizzaCustomizationOptions();
   });
 
-  $("#signaturePizzas button").click(function(event) {
-    var signaturePizzaName = $(event.target).attr("id").substring(15);
+  $("div.container-btn-size button").click(function(event) {
+    //id="signaturePizza:${pizza.name}:S"
+    var idArray = $(event.target).attr("id").split(":");
+    var signaturePizzaSize = idArray[2];
+    var signaturePizzaName = idArray[1];
     var signaturePizza = pizzeria.getPizzaByName(signaturePizzas, signaturePizzaName);
 
-    orderTracker.addPizza(
-        new Pizza([].concat(signaturePizza.ingredients)),
-        "Large"
-    );
+    var pizza = new Pizza([].concat(signaturePizza.ingredients));
+    pizza.name = signaturePizza.name;
+    pizza.description = signaturePizza.description;
+    pizza.imagePath = signaturePizza.imagePath;
+    orderTracker.trackPizzaOrder(pizza, signaturePizzaSize);
 
     console.log(orderTracker);
     contentPresentation.showPizzaCustomizationOptions();
@@ -44,7 +51,7 @@ ContentPresentation.prototype.showSignaturePizzas = function() {
       <div class="col-sm-5" >
           <div class="img-pizza-title" style="text-align:center;">${pizza.name}</div>
           <img class="img-pizza" src="${pizza.imagePath}" alt="A photo of a plain pizza">
-          <div id="signaturePizzas" style="margin-left:5%;margin-right:5%;">
+          <div class="container-btn-size" style="margin-left:5%;margin-right:5%;">
             <button id="signaturePizza:${pizza.name}:S" class="btn-size btn btn-success">sm.</button>
             <button id="signaturePizza:${pizza.name}:M" class="btn-size btn btn-success">med.</button>
             <button id="signaturePizza:${pizza.name}:L" class="btn-size btn btn-success">lg.</button>
